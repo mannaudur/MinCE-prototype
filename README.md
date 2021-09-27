aacb5b9d7b5f871a798166e5e32a2d32c24adfd9
Þórhallur Auður Helgason <thorhallurh@gmail.com>
2021-09-27 23:28:51 +0000

    README includes link to reverse dictionaries and feature files (still uploading)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mince
 MinCE - Minhash with Cluster Extensions
+
+Download reverse dictionaries and feature files at https://www.dropbox.com/sh/7pjw8hc40aeb2f0/AADqfonag4GdioiUXbCJNCEba?dl=0.
--- setup/get_revDicts.py
+++ setup/get_revDicts.py
@@ -19,33 +19,28 @@ import os
 def main(args):
     hashlocator = args['<input_file>']
     data = pd.read_csv(hashlocator, header = None)
-    division = int(args['-d'])
-    burst = len(data[0])/division
-    if int(args['-s']):
-        burst = int(args['-s'])
-        division = int(np.ceil(len(data[0])/burst))
     
     os.system('mkdir revDicts')
 
     rev_dict = []
     for i in range(len(data[0])):
         kash = list(data[0][i].split(' '))[:-1]
-        rev_dict.append([int(kash[0]),";".join(kash[1::])])
+        rev_dict.append(",".join(kash[0],";".join(kash[1::])))
     rev_dict.sort()
 
-    for i in range(division):
+    for i in range(10,100):
         dictName = 'revDicts/revDict'+str(i)+'.txt'
         with open(dictName, 'w') as f:
-            for item in rev_dict[int(np.ceil(i*burst)):min(len(rev_dict), int(np.ceil((i+1)*burst)))]:
-                item[0] = str(item[0])
-                f.write("%s\n" % ','.join(item))
-            f.close()
-                
-    with open('revDicts/revDict_companion.txt', 'w') as f:
-        for j in range(division):
-            revDict_j = pd.read_csv('revDicts/revDict'+str(j)+'.txt', header = None, sep=',')
-            f.write(str(j) + " %s\n" % list(revDict_j[0])[-1])
-        f.close()
+            for item in rev_dict:
+                if int(item[0:2]) < i:
+                    print('Something fishy going on...')
+                    print(int(item[0:2]))
+                    print(i)
+                if int(item[0:2]) > i:
+                    f.close()
+                    break
+                else:
+                    f.write("%s\n" % item)
 
 if __name__ == "__main__":
     args = docopt(__doc__)
